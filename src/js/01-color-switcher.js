ad19545bc47abb36fe81337ac4a709b2f98c70d5
LudaFesenko <108350058+LudaFesenko@users.noreply.github.com>
2022-12-21 22:08:47 +0200

    task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+let timerId = null;
+
+stopBtn.setAttribute('disabled', true);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function setBodyColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+startBtn.addEventListener('click', () => {
+  startBtn.setAttribute('disabled', true);
+  stopBtn.removeAttribute('disabled');
+
+  setBodyColor();
+
+  timerId = setInterval(() => {
+    setBodyColor();
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  startBtn.removeAttribute('disabled');
+  stopBtn.setAttribute('disabled', true);
+});
