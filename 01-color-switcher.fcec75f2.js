c0033fce916867765818e8c23edd007ed7883281
LudaFesenko <108350058+LudaFesenko@users.noreply.github.com>
2022-12-26 09:52:44 +0000

    Deploying to gh-pages from @ LudaFesenko/goit-js-hw-09@b6b99ec9b9f172a50995477305d1533378695ab4 🚀

---
+++ 01-color-switcher.fcec75f2.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let d=null;function r(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}e.setAttribute("disabled",!0),t.addEventListener("click",(()=>{t.setAttribute("disabled",!0),e.removeAttribute("disabled"),r(),d=setInterval((()=>{r()}),1e3)})),e.addEventListener("click",(()=>{clearInterval(d),t.removeAttribute("disabled"),e.setAttribute("disabled",!0)}));
+//# sourceMappingURL=01-color-switcher.fcec75f2.js.map
